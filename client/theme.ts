1d5f79138886f404510b0f389ede1489ca4d97ab
Hogan McDonald <hogan.developer@gmail.com>
2019-09-20 22:03:14 -0500

    fixes dark accent color in theme

--- client/theme.ts
+++ client/theme.ts
@@ -1,11 +1,11 @@
-const theme =  {
+const theme = {
   colors: {
     background: '#121212',
     black: '#000000',
     white: '#e2e2e2',
     navigation: '#1f1f1f',
     accent: '#ba8af9',
-    accentDark: '#ba8af9',
+    accentDark: '#3d2d51',
 
     textPrimary: '#e2e2e2',
     textSecondary: '#9b9b9b',
