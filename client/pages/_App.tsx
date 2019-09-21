5d395fb7da56bba399e53251ae5732fc9d5f0e5e
Hogan McDonald <hogan.developer@gmail.com>
2019-09-20 22:03:00 -0500

    adds global style to body element

--- client/pages/_App.tsx
+++ client/pages/_App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import App from 'next/app'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { Provider } from 'react-redux'
 
 import theme from '../theme'
 import store from '../store'
+
+const PageStyles = createGlobalStyle`
+  body, html {
+    margin: 0;
+    height: 100%;
+  }
+`
 
 export default class MyApp extends App {
   render() {
@@ -12,7 +19,10 @@ export default class MyApp extends App {
     return (
       <Provider store={store}>
         <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
+          <>
+            <PageStyles />
+            <Component {...pageProps} />
+          </>
         </ThemeProvider>
       </Provider>
     )
