4d3fed9141c9cd0f644450df7a47993bf71434c8
Hogan McDonald <hogan.developer@gmail.com>
2019-09-20 22:33:00 -0500

    uses login button in index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
-import Head from 'next/head'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const Background = styled.body`
+import SpotifyLogo from '../components/SpotifyLogo'
+import Button from '../components/Button'
+
+const Background = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.background};
-  color: ${({theme}) => theme.colors.textPrimary};
+  color: ${({ theme }) => theme.colors.textPrimary};
 `
 
-const Home = () => <Background>record app</Background>
+const SignInLogo = styled(SpotifyLogo)`
+  height: 100%;
+  margin-right: 0.5rem;
+`
+
+const SignInButton = styled(Button)`
+  padding-right: 0.75rem;
+  color: ${({ theme }) => theme.colors.textPrimary};
+`
+
+const Home = () => (
+  <Background>
+    <h1>Log in to get started.</h1>
+    <SignInButton label="Log in sith Spotify">
+      <SignInLogo />
+      Log in with Spotify
+    </SignInButton>
+  </Background>
+)
 
 export default Home
