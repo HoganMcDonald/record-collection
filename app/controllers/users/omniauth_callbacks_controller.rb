2b9f7255fc9bcaba3066a7ce578151f4b256a876
Hogan McDonald <hogan.developer@gmail.com>
2019-10-25 20:16:20 -0500

    revises omniauth callback to pass token in params

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,20 +1,43 @@
 module Users
   class OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
-    def omniauth_success
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+    after_action :update_auth_header
 
-      if @user.persisted?
-        sign_in @user
-        redirect_to "#{RecordCollection::CLIENT_HOST}/new"
-      else
-        session["devise.spotify_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
+    def omniauth_success
+      get_resource_from_auth_hash
+      set_token_on_resource
+      create_auth_params
+
+      sign_in(:user, @resource, store: false, bypass: false)
+      @resource.save!
+
+      yield @resource if block_given?
+
+      redirect_to "#{RecordCollection::CLIENT_HOST}/#{token_to_params}"
     end
 
     def failure
       redirect_to "#{RecordCollection::CLIENT_HOST}/?error=#{CGI.escape('Unable to sign in due to Spotify error.')}"
+    end
+
+    protected
+
+    def resource_class
+      User
+    end
+
+    def auth_hash
+      request.env["omniauth.auth"]
+    end
+
+    private
+
+    def token_to_params
+      [
+        "?uid=#{CGI.escape @auth_params[:uid]}",
+        "&expiry=#{@auth_params[:expiry]}",
+        "&client=#{CGI.escape @auth_params[:client_id]}",
+        "&access-token=#{CGI.escape @auth_params[:auth_token]}",
+      ].join('')
     end
   end
 end
