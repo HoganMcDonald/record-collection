81f7798fe12d2060f9c5427ef92d59b7272bcbb7
Hogan McDonald <hogan.developer@gmail.com>
2019-10-26 19:59:24 -0500

    updates token on api requests

--- client/lib/apiHelpers.ts
+++ client/lib/apiHelpers.ts
@@ -5,7 +5,7 @@ import { apiUrl } from '../constants'
 import { useAuth } from '../reducers/auth'
 
 export const useApiRequests = () => {
-  const { clearAuthToken, authToken } = useAuth()
+  const { clearAuthToken, authToken, setAuthToken } = useAuth()
 
   const getHeadersFromToken = (authToken: AuthToken) => {
     const authHeaders = new Headers()
@@ -24,6 +24,17 @@ export const useApiRequests = () => {
     }
   }
 
+  const updateAuthTokenFromHeaders = (response: fetch.IsomorphicResponse) => {
+    setAuthToken({
+      uid: response.headers.get('uid') || authToken.uid,
+      client: response.headers.get('client') || authToken.client,
+      expiry: response.headers.get('expiry') || authToken.expiry,
+      'access-token':
+        response.headers.get('access-token') || authToken['access-token'],
+      'token-type': 'Bearer',
+    })
+  }
+
   const parseResponseBody = (response: fetch.IsomorphicResponse) =>
     response.json()
 
@@ -34,6 +45,7 @@ export const useApiRequests = () => {
       headers: getHeadersFromToken(authToken),
     })
 
+    updateAuthTokenFromHeaders(response)
     parseErrorResponses(response)
     return await parseResponseBody(response)
   }
