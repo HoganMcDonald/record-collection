5c04062d9b7b9befb2a009d7b277d47004ebfa7e
Hogan McDonald <hogan.developer@gmail.com>
2019-11-23 23:44:29 -0600

    creates a base button reset style

--- client/components/styled.tsx
+++ client/components/styled.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Background = styled.div`
   height: 100%;
@@ -9,4 +9,29 @@ export const Background = styled.div`
   align-items: center;
   background-color: ${({ theme }) => theme.colors.background};
   color: ${({ theme }) => theme.colors.textPrimary};
+`
+
+interface ResetButtonProps {
+  disabled?: boolean
+}
+
+const disabledStyles = css`
+  pointer-events: none;
+`
+
+export const ResetButton = styled.button<ResetButtonProps>`
+  -webkit-appearance: button;
+  background: none;
+  border: none;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  font-family: inherit;
+  font-size: 100%;
+  line-height: 0;
+  line-height: 1.15;
+  margin: 0;
+  outline: none;
+  overflow: visible;
+  padding: 0;
+  text-transform: none;
+  ${({ disabled }) => disabled && `${disabledStyles}`}
 `
