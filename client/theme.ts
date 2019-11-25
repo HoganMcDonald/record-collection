a43910e4322bbd49cea47ae890fb43b72e2c2992
Hogan McDonald <hogan.developer@gmail.com>
2019-11-24 20:45:33 -0600

    adds new colors from sketchfile

--- client/theme.ts
+++ client/theme.ts
@@ -2,14 +2,16 @@ import Typography from 'typography'
 
 const theme = {
   colors: {
-    background: '#121212',
     black: '#000000',
     white: '#e2e2e2',
     navigation: '#1f1f1f',
     accent: '#ba8af9',
     accentDark: '#3d2d51',
 
+    background: '#11051B',
+    sideBar: '#121212',
     playBar: '#1c1c1c',
+    progressBar: '#B620E0',
     icons: '#FFFFFF',
 
     textPrimary: '#e2e2e2',
