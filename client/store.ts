ccac8f351dc65e9a8c056bced27b8fb8b3c070ee
Hogan McDonald <hogan.developer@gmail.com>
2019-11-27 20:01:58 -0600

    adds player to store

--- client/store.ts
+++ client/store.ts
@@ -2,16 +2,19 @@ import { configureStore } from 'redux-starter-kit'
 
 import userReducer, { User } from './reducers/users'
 import authReducer, { Auth } from './reducers/auth'
+import playerReducer, { Player } from './reducers/player'
 
 export type State = {
   auth: Auth
   user: User
+  player: Player
 }
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     auth: authReducer,
+    player: playerReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 })
