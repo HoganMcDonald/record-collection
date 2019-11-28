02b67b292c3d0602252ed07f1972b4f154cf0ec3
Hogan McDonald <hogan.developer@gmail.com>
2019-11-27 19:05:53 -0600

    adds player status to omniauth scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     playlist-read-private
     user-read-private
     user-read-email
+    user-read-playback-state
+    user-modify-playback-state
+    user-read-currently-playing
   ).join(' ')
 end
