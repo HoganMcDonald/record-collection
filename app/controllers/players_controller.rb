7b9418e59e4b0aa16330be97c1e231594795268b
Hogan McDonald <hogan.developer@gmail.com>
2019-11-28 09:35:59 -0600

    handles play and pause in update method for player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,11 +3,26 @@ class PlayersController < ApplicationController
 
   def show
     @player = spotify_client.player_status!
+    head :no_content if @player.nil?
+  end
+
+  def update
+    @player = nil
+
+    spotify_client.pause! if update_params[:pause].present?
+
+    spotify_client.play! if update_params[:play].present?
+
+    head :no_content if @player.nil?
   end
 
   private
 
   def spotify_client
     @spotify_client ||= ::Spotify.new(@current_user)
+  end
+
+  def update_params
+    params.require(:player).permit(:play, :pause)
   end
 end
