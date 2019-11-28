66dcf80e42a90ebba32e5b5df78cf082ca23fd58
Hogan McDonald <hogan.developer@gmail.com>
2019-11-28 01:11:25 -0600

    adds pause icon

--- client/components/icons.tsx
+++ client/components/icons.tsx
@@ -8,12 +8,7 @@ interface IconProps {
 export const NextSong: React.FC<IconProps> = ({ className }) => {
   const { colors } = React.useContext(ThemeContext)
   return (
-    <svg
-      className={className}
-      width="12px"
-      height="16px"
-      viewBox="0 0 12 16"
-      version="1.1">
+    <svg className={className} width="12px" height="16px" viewBox="0 0 12 16">
       <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g transform="translate(-547.000000, -602.000000)">
           <g transform="translate(-1.000000, 580.000000)">
@@ -23,6 +18,28 @@ export const NextSong: React.FC<IconProps> = ({ className }) => {
               fillRule="nonzero">
               <g transform="translate(87.000000, 12.000000) rotate(180.000000) translate(-87.000000, -12.000000) translate(81.000000, 4.000000)">
                 <path d="M0,15.5712139 L0,0.428786067 C0,0.193078464 0.2025,0.000226788261 0.45,0.000226788261 L2.25,0.000226788261 C2.4975,0.000226788261 2.7,0.193078464 2.7,0.428786067 L2.7,6.72860747 L10.03125,0.26450501 C10.80375,-0.346191962 12,0.164507845 12,1.14305153 L12,14.8569485 C12,15.8354922 10.80375,16.346192 10.03125,15.735495 L2.7,9.31067713 L2.7,15.5712139 C2.7,15.8069215 2.4975,15.9997732 2.25,15.9997732 L0.45,15.9997732 C0.2025,15.9997732 0,15.8069215 0,15.5712139 Z"></path>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export const Pause: React.FC<IconProps> = ({ className }) => {
+  const { colors } = React.useContext(ThemeContext)
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 24 24">
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g transform="translate(-500.000000, -598.000000)">
+          <g transform="translate(-1.000000, 580.000000)">
+            <g
+              transform="translate(467.000000, 18.000000)"
+              fill={colors.white}
+              fillRule="nonzero">
+              <g transform="translate(34.000000, 0.000000)">
+                <path d="M7.71428571,24 L2.57142857,24 C1.15178571,24 0,22.8482143 0,21.4285714 L0,2.57142857 C0,1.15178571 1.15178571,0 2.57142857,0 L7.71428571,0 C9.13392857,0 10.2857143,1.15178571 10.2857143,2.57142857 L10.2857143,21.4285714 C10.2857143,22.8482143 9.13392857,24 7.71428571,24 Z M24,21.4285714 L24,2.57142857 C24,1.15178571 22.8482143,0 21.4285714,0 L16.2857143,0 C14.8660714,0 13.7142857,1.15178571 13.7142857,2.57142857 L13.7142857,21.4285714 C13.7142857,22.8482143 14.8660714,24 16.2857143,24 L21.4285714,24 C22.8482143,24 24,22.8482143 24,21.4285714 Z"></path>
               </g>
             </g>
           </g>
