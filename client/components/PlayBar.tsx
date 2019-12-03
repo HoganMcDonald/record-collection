c77ee14dd9cad1b279cf6c52a4232866348de957
Hogan McDonald <hogan.developer@gmail.com>
2019-12-02 20:19:56 -0600

    removes unused import

--- client/components/PlayBar.tsx
+++ client/components/PlayBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import styled, { keyframes, css } from 'styled-components'
+import styled from 'styled-components'
 
 import { Play, Speaker, PreviousSong, NextSong, Pause } from './icons'
 import { ResetButton } from './styled'
