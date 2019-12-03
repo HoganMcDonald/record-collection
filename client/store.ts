569d27ca2f2b9d6bdefa72bcb692d26e19876758
Hogan McDonald <hogan.developer@gmail.com>
2019-12-02 20:20:04 -0600

    adds toast reducer

--- client/store.ts
+++ client/store.ts
@@ -3,11 +3,13 @@ import { configureStore } from 'redux-starter-kit'
 import userReducer, { User } from './reducers/users'
 import authReducer, { Auth } from './reducers/auth'
 import playerReducer, { Player } from './reducers/player'
+import toastReducer, { Toasts } from './reducers/toast'
 
 export type State = {
   auth: Auth
   user: User
   player: Player
+  toasts: Toasts
 }
 
 const store = configureStore({
@@ -15,6 +17,7 @@ const store = configureStore({
     user: userReducer,
     auth: authReducer,
     player: playerReducer,
+    toasts: toastReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 })
