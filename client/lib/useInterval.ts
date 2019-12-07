d8d6fadcd7dccd3f32a6869241e54bdf113d0a39
Hogan McDonald <hogan.developer@gmail.com>
2019-12-06 23:51:06 -0600

    refines useInterval hook

--- client/lib/useInterval.ts
+++ client/lib/useInterval.ts
@@ -1,16 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 export const useInterval = (callback: () => void, delay: number) => {
   const savedCallback = useRef(callback)
-  const savedInterval = useRef(delay)
 
   useEffect(() => {
     savedCallback.current = callback
   }, [callback])
-
-  useEffect(() => {
-    savedInterval.current = delay
-  }, [delay])
 
   useEffect(() => {
     const tick = () => {
