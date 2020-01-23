4df4d17a72682e40005c353d7f1ab67ccd401a2c
Hogan McDonald <hogan.developer@gmail.com>
2020-01-22 23:18:43 -0600

    uses carousel for albums

--- client/components/Browse.tsx
+++ client/components/Browse.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react'
 
+import { useSearch } from '../reducers/search'
 import { Container } from './styled'
 import SearchBar from './SearchBar'
+import Carousel from './Carousel'
 
 const Browse: React.FC = () => {
+  const { searchResults } = useSearch()
+
   return (
     <Container>
       <SearchBar />
+      {searchResults.albums.length > 0 && (
+        <Carousel title="Albums" items={searchResults.albums} />
+      )}
     </Container>
   )
 }
