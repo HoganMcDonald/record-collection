90f6384f005981d8599a6104bb49bd71271c9e07
Hogan McDonald <hogan.developer@gmail.com>
2020-02-14 18:04:55 -0600

    adds method for inserting album to collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -4,12 +4,24 @@ class Collection < ActiveRecord::Base
   belongs_to :user
 
   has_many :albums_collections
-  has_many :albums, through: :albums_collection
+  has_many :albums, -> { order 'albums_collections.position' }, through: :albums_collections
 
   validates :user_id, presence: true
   validates :name, presence: true
 
   validate :ensure_one_default_per_user, if: :default
+
+  def add_album!(uri)
+    ActiveRecord::Base.transaction do
+      album = Album.find_or_create_by!(spotify_uri: uri) do |new_album|
+        response = spotify_client.from_uri! uri
+        new_album.name = response.body.fetch('name')
+        new_album.artist_name = response.body.fetch('artists').first.fetch('name')
+      end
+
+      albums_collections.create! album: album, position: albums_collections.count + 1
+    end
+  end
 
   private
 
@@ -17,5 +29,9 @@ class Collection < ActiveRecord::Base
     return if user.collections.where(default: true).count <= 1
 
     errors.add(:default, 'Users can only have 1 default collection')
+  end
+
+  def spotify_client
+    @spotify_client ||= ::Spotify.new(user)
   end
 end
