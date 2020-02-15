5b79f57eeed0f79dad2ad42c1de99ea8c9df55d8
Hogan McDonald <hogan.developer@gmail.com>
2020-02-14 22:38:12 -0600

    adds authed user trait

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email { 'hogan@example.com' }
+    email { "#{SecureRandom.hex(10)}@example.com" }
     name { 'Hogan' }
     image { 'https://www.fillmurray.com/200/200' }
+
+    trait :authed do
+      spotify_access_token { SecureRandom.hex 60 }
+      spotify_refresh_token { SecureRandom.hex 60 }
+      spotify_token_expires_at { Time.zone.now + 1.hour }
+    end
   end
 end
