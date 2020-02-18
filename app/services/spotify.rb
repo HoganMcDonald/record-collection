2270d5386ef3b3819ead4cc50fbdd5a1e34a9431
Hogan McDonald <hogan.developer@gmail.com>
2020-02-17 22:15:59 -0600

    from uri now accepts array of uris

--- app/services/spotify.rb
+++ app/services/spotify.rb
@@ -1,5 +1,6 @@
 class MissingAuthTokenError < StandardError; end
 class UnsupportedUriTypeError < StandardError; end
+class MixedUriTypeError < StandardError; end
 
 class Spotify
   def initialize(user)
@@ -32,25 +33,54 @@ class Spotify
     get!("/search?q=#{ERB::Util.url_encode(q)}&type=album,artist,track")
   end
 
-  def from_uri!(uri)
-    components = parse_uri(uri)
+  def from_uris!(uris)
+    entity, id, ids = nil, nil, nil
 
-    unless components[:entity].present? && components[:id].present?
-      raise UnsupportedUriTypeError, "Invalid URI provided \"#{uri}\""
+    if uris.is_a?(Array)
+      entity = parse_uri!(uris.first)[:entity]
+      ids = uris.map { |uri|
+        parsed_uri = parse_uri! uri
+
+        raise MixedUriTypeError, "entity should be #{entity}, recieved uri: #{uri}" if parsed_uri[:entity] != entity
+
+        parsed_uri[:id]
+      }
+                .each_slice(20) # 20 is the limit for a single request
+                .to_a
+                .map { |id_list| id_list.join ',' }
+    else
+      components = parse_uri!(uris)
+      entity = components[:entity]
+      id = components[:id]
     end
 
-    if components[:entity] == 'album'
-      get!("/albums/#{components[:id]}")
+    multi = !ids.nil?
+
+    if entity == 'album'
+      if multi
+        ids.reduce([]) do |acc, id_list|
+          response = get_albums! id_list
+          acc.concat response.body['albums']
+        end
+      else
+        get_album!(id)
+      end
     else
       raise UnsupportedUriTypeError,
-            "unable to find entity for uri #{components[:entity]}."
+            "unable to fetch entity for uri #{components[:entity]}."
     end
   end
+  alias_method :from_uri!, :from_uris!
 
   private
 
-  def parse_uri(uri)
+  def parse_uri!(uri)
     components = uri.split ':'
+
+    unless components.length == 3
+      raise UnsupportedUriTypeError, "Invalid URI provided \"#{uri}\""
+    end
+
     {
       entity: components[1],
       id: components[2]
@@ -113,5 +143,13 @@ class Spotify
       spotify_access_token: response.body['access_token'],
       spotify_token_expires_at: Time.now + response.body['expires_in'].to_i)
     set_token!
+  end
+
+  def get_album!(id)
+    get!("/albums/#{id}")
+  end
+
+  def get_albums!(ids)
+    get!("/albums?ids=#{ids}")
   end
 end
