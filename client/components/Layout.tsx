1bc331bd9f57182317a74e713acf1acd704269ee
Hogan McDonald <hogan.developer@gmail.com>
2020-02-20 21:13:50 -0600

    creates sidebar component

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+import { useAuth } from '../reducers/auth'
+import { useUser } from '../reducers/users'
 import { Background } from './styled'
 import PlayBar from './PlayBar'
 import Toasts from './Toasts'
+import Sidebar from './Sidebar'
+import { useCollections } from '../reducers/collection'
 
 const FullScreenContainer = styled.div`
   min-width: 1000px;
@@ -14,21 +18,30 @@ const FullScreenContainer = styled.div`
   grid-template-rows: 1fr 60px;
 `
 
-const SideBar = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: ${({ theme }) => theme.colors.sideBar};
-`
-
 const ScrollableBackground = styled(Background)`
   overflow-x: hidden;
   overflow-y: scroll;
 `
 
 const Layout: React.FC = ({ children }) => {
+  const { authToken } = useAuth()
+  const { getMe } = useUser()
+  const { getDefaultCollection } = useCollections()
+
+  React.useEffect(() => {
+    getMe()
+    getDefaultCollection()
+  }, [])
+
+  React.useEffect(() => {
+    if (!authToken) {
+      getMe()
+    }
+  }, [authToken])
+
   return (
     <FullScreenContainer>
-      <SideBar />
+      <Sidebar />
       <ScrollableBackground>{children}</ScrollableBackground>
       <PlayBar />
       <Toasts />
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,28 +1,10 @@
 import { NextPage } from 'next'
 import React from 'react'
 
-import { useAuth } from '../reducers/auth'
-import { useUser } from '../reducers/users'
 import Layout from '../components/Layout'
 import Browse from '../components/Browse'
-import { useCollections } from '../reducers/collection'
 
 const Home: NextPage = () => {
-  const { authToken } = useAuth()
-  const { getMe } = useUser()
-  const { getDefaultCollection } = useCollections()
-
-  React.useEffect(() => {
-    getMe()
-    getDefaultCollection()
-  }, [])
-
-  React.useEffect(() => {
-    if (!authToken) {
-      getMe()
-    }
-  }, [authToken])
-
   return (
     <Layout>
       <Browse />
