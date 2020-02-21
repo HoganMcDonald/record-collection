ac780508311636eec074d87d166efa0a37d34ed5
Hogan McDonald <hogan.developer@gmail.com>
2020-02-20 21:38:19 -0600

    update app host

--- client/constants.ts
+++ client/constants.ts
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://hoganmcdonald.com'
+    ? 'https://stream-collector-rails.herokuapp.com'
     : 'http://localhost:3001'
 
 export const localStorageKey = 'stream_collector'
