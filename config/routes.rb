16beecebbb26096780f50df3842d8bb5167eed6e
Hogan McDonald <hogan.developer@gmail.com>
2020-02-26 23:35:47 -0600

    adds namespacing for controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/me', to: 'users#me'
-  get '/search', to: 'search#show'
-  resource :player, only: [:show, :update]
-  resource :collection, only: [:show, :update]
+  namespace :api do
+    get '/me', to: 'users#me'
+    get '/search', to: 'search#show'
+    resource :player, only: [:show, :update]
+    resource :collection, only: [:show, :update]
+  end
 end
