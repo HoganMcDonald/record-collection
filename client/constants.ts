81feb23c3d664922c92c8bc465c25c66474ff64b
Hogan McDonald <hogan.developer@gmail.com>
2020-02-27 12:20:23 -0600

    removes client directory

--- client/constants.ts
+++
@@ -1,6 +0,0 @@
-export const apiUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://stream-collector-rails.herokuapp.com'
-    : 'http://localhost:3001'
-
-export const localStorageKey = 'stream_collector'
