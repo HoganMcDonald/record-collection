0288c5286895fd87b12eafb813588c080d15d54b
Hogan McDonald <hogan.developer@gmail.com>
2020-02-28 15:30:02 -0600

    adds typography

--- app/javascript/packs/app.tsx
+++ app/javascript/packs/app.tsx
@@ -2,14 +2,17 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
+import { TypographyStyle, GoogleFont } from 'react-typography'
 
 import Router from './components/Router'
 import store from './store'
-import theme from './theme'
+import theme, { typography } from './theme'
 
 const App: React.FC = () => (
   <Provider store={store}>
     <ThemeProvider theme={theme}>
+      <TypographyStyle typography={typography} />
+      <GoogleFont typography={typography} />
       <Router />
     </ThemeProvider>
   </Provider>
