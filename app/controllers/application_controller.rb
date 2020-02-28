0dcbf7ebeeafc8bda195276638457635381a7ecb
Hogan McDonald <hogan.developer@gmail.com>
2020-02-27 22:03:15 -0600

    adds method for preloading redux state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  include Devise::Controllers::Helpers
+
+  def redux_state
+    initial_state = {}
+
+    if current_user.present?
+      @collection = @current_user.default_collection
+      @albums = @collection.spotify_albums! unless @collection.albums.empty?
+
+      initial_state[:user] = JSON.parse render_to_string(
+        template: 'api/users/me'
+      )
+
+      initial_state[:collections] = {
+        defaultCollection: JSON.parse(
+          render_to_string(
+            template: 'api/collections/show'
+          )
+        )
+      }
+    end
+
+    initial_state.to_json
+  end
+  helper_method :redux_state
 end
