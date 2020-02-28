3a030b8a4afab2a5c10f3f6bf614214398714fd7
Hogan McDonald <hogan.developer@gmail.com>
2020-02-27 18:13:43 -0600

    renders redux object on window

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'home#index'
+  get '/collection', to: 'home#index'
 
   namespace :api do
     get '/me', to: 'users#me'
