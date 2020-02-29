be372007c22cef572c0f0a25d6c26450c9597b6a
Hogan McDonald <hogan.developer@gmail.com>
2020-02-28 23:29:31 -0600

    removes unused constant

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,4 @@
 module RecordCollection
-  CLIENT_HOST = Rails.env == 'production' ? ENV['CLIENT_HOST'] : 'http://localhost:3000'
-
   # Spotify
   SPOTIFY_CLIENT_ID = Rails.application.credentials.dig(:spotify, :client_id)
   SPOTIFY_CLIENT_SECRET = Rails.application.credentials.dig(:spotify, :client_secret)
