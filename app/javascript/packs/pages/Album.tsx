72d1e7f8f67b8d7e7d38def75a21ee093322ba65
Hogan McDonald <hogan.developer@gmail.com>
2020-03-02 22:45:29 -0600

    adds loader to album detail page

--- app/javascript/packs/pages/Album.tsx
+++ app/javascript/packs/pages/Album.tsx
@@ -4,17 +4,21 @@ import { useParams } from 'react-router-dom'
 import Layout from '../components/Layout'
 import { Container } from '../components/styled'
 import { useAlbums } from '../reducers/albums'
+import Loader from '../components/Loader'
 
 const Album: React.FC = () => {
   const { albumId } = useParams<{ albumId: string }>()
   const { loading, album } = useAlbums(albumId)
 
   return (
-    <Layout Header={() => <h1>Album</h1>}>
-      <Container>
-        {albumId} <br />- Loading: {loading.toString()}
-        <br />- Album Name: {album && album.name}
-      </Container>
+    <Layout
+      Header={() => (album ? <h1>{album.name}</h1> : <Loader height={30} />)}>
+      {loading ? (
+        <Container>
+          {albumId} <br />- Loading: {loading.toString()}
+          <br />- Album Name: {album && album.name}
+        </Container>
+      ) : null}
     </Layout>
   )
 }
